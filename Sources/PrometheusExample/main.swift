69a0ade83af2d2f030e3f06b77712d580fc56c7a
MrLotU <j.koopman@jarict.nl>
2019-06-05 11:00:43 -0700

    Imp examples

--- Sources/PrometheusExample/main.swift
+++ Sources/PrometheusExample/main.swift
@@ -1,8 +1,5 @@
 import PrometheusMetrics
 import Foundation
-//import Metrics
-
-//let counter = Counter(label: <#T##String#>, dimensions: <#T##[(String, String)]#>)
 
 let myProm = PrometheusClient()
 
@@ -18,25 +15,37 @@ for _ in 0...Int.random(in: 10...100) {
     c.increment()
 }
 
-//let r = Recorder(label: "recorder")
-//for _ in 0...Int.random(in: 100...500_000) {
-//   r.record(Double.random(in: 0...20))
-//}
-//
-//let g = Gauge(label: "non_agg_recorder")
-//for _ in 0...Int.random(in: 100...500_000) {
-//    g.record(Double.random(in: 0...20))
-//}
-//
-//let t = Timer(label: "timer")
-//for _ in 0...Int.random(in: 100...500_000) {
-//    t.recordMicroseconds(Double.random(in: 20...150))
-//}
+for _ in 0...Int.random(in: 100...500_000) {
+    let r = Recorder(label: "recorder")
+    r.record(Double.random(in: 0...20))
+}
+
+for _ in 0...Int.random(in: 100...500_000) {
+    let g = Gauge(label: "non_agg_recorder")
+    g.record(Double.random(in: 0...20))
+}
+
+for _ in 0...Int.random(in: 100...500_000) {
+    let t = Timer(label: "timer")
+    t.recordMicroseconds(Double.random(in: 20...150))
+}
+
+for _ in 0...Int.random(in: 100...500_000) {
+    let r = Recorder(label: "recorder", dimensions: [("abc", "123")])
+    r.record(Double.random(in: 0...20))
+}
+
+for _ in 0...Int.random(in: 100...500_000) {
+    let g = Gauge(label: "non_agg_recorder", dimensions: [("abc", "123")])
+    g.record(Double.random(in: 0...20))
+}
+
+for _ in 0...Int.random(in: 100...500_000) {
+    let t = Timer(label: "timer", dimensions: [("abc", "123")])
+    t.recordMicroseconds(Double.random(in: 20...150))
+}
 
 
-//let x = myProm.makeCounter(label: "test", dimensions: [])
-//x.increment(by: 12)
-//
 struct MyCodable: MetricLabels {
    var thing: String = "*"
 }
@@ -50,76 +59,76 @@ counter.inc(5)
 counter.inc(Int.random(in: 0...100), codable2)
 counter.inc(Int.random(in: 0...100), codable1)
 
-//let gauge = myProm.createGauge(forType: Int.self, named: "my_gauge", helpText: "Just a gauge", initialValue: 12, withLabelType: MyCodable.self)
-//
-//gauge.inc(100)
-//gauge.inc(Int.random(in: 0...100), codable2)
-//gauge.inc(Int.random(in: 0...100), codable1)
-//
-//struct HistogramThing: HistogramLabels {
-//   var le: String = ""
-//   let route: String
-//
-//   init() {
-//       self.route = "*"
-//   }
-//
-//   init(_ route: String) {
-//       self.route = route
-//   }
-//}
-//
-//let histogram = myProm.createHistogram(forType: Double.self, named: "my_histogram", helpText: "Just a histogram", labels: HistogramThing.self)
-//
-//for _ in 0...Int.random(in: 10...50) {
-//   histogram.observe(Double.random(in: 0...1))
-//}
-//
-//for _ in 0...Int.random(in: 10...50) {
-//   histogram.observe(Double.random(in: 0...1), HistogramThing("/test"))
-//}
-//
-//struct SummaryThing: SummaryLabels {
-//   var quantile: String = ""
-//   let route: String
-//
-//   init() {
-//       self.route = "*"
-//   }
-//
-//   init(_ route: String) {
-//       self.route = route
-//   }
-//}
-//
-//let summary = myProm.createSummary(forType: Double.self, named: "my_summary", helpText: "Just a summary", labels: SummaryThing.self)
-//
-//for _ in 0...Int.random(in: 100...1000) {
-//   summary.observe(Double.random(in: 0...10000))
-//}
-//
-//for _ in 0...Int.random(in: 100...1000) {
-//   summary.observe(Double.random(in: 0...10000), SummaryThing("/test"))
-//}
-//
-//struct MyInfoStruct: MetricLabels {
-//   let version: String
-//   let major: String
-//
-//   init() {
-//       self.version = "1.0.0"
-//       self.major = "1"
-//   }
-//
-//   init(_ v: String, _ m: String) {
-//       self.version = v
-//       self.major = m
-//   }
-//}
-//
-//let info = myProm.createInfo(named: "my_info", helpText: "Just some info", labelType: MyInfoStruct.self)
-//
-//info.info(MyInfoStruct("2.0.0", "2"))
+let gauge = myProm.createGauge(forType: Int.self, named: "my_gauge", helpText: "Just a gauge", initialValue: 12, withLabelType: MyCodable.self)
+
+gauge.inc(100)
+gauge.inc(Int.random(in: 0...100), codable2)
+gauge.inc(Int.random(in: 0...100), codable1)
+
+struct HistogramThing: HistogramLabels {
+   var le: String = ""
+   let route: String
+
+   init() {
+       self.route = "*"
+   }
+
+   init(_ route: String) {
+       self.route = route
+   }
+}
+
+let histogram = myProm.createHistogram(forType: Double.self, named: "my_histogram", helpText: "Just a histogram", labels: HistogramThing.self)
+
+for _ in 0...Int.random(in: 10...50) {
+   histogram.observe(Double.random(in: 0...1))
+}
+
+for _ in 0...Int.random(in: 10...50) {
+   histogram.observe(Double.random(in: 0...1), HistogramThing("/test"))
+}
+
+struct SummaryThing: SummaryLabels {
+   var quantile: String = ""
+   let route: String
+
+   init() {
+       self.route = "*"
+   }
+
+   init(_ route: String) {
+       self.route = route
+   }
+}
+
+let summary = myProm.createSummary(forType: Double.self, named: "my_summary", helpText: "Just a summary", labels: SummaryThing.self)
+
+for _ in 0...Int.random(in: 100...1000) {
+   summary.observe(Double.random(in: 0...10000))
+}
+
+for _ in 0...Int.random(in: 100...1000) {
+   summary.observe(Double.random(in: 0...10000), SummaryThing("/test"))
+}
+
+struct MyInfoStruct: MetricLabels {
+   let version: String
+   let major: String
+
+   init() {
+       self.version = "1.0.0"
+       self.major = "1"
+   }
+
+   init(_ v: String, _ m: String) {
+       self.version = v
+       self.major = m
+   }
+}
+
+let info = myProm.createInfo(named: "my_info", helpText: "Just some info", labelType: MyInfoStruct.self)
+
+info.info(MyInfoStruct("2.0.0", "2"))
 
 let metrics = try! MetricsSystem.prometheus().getMetrics()
 print(metrics)
