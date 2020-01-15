275d379fb4e57a227a159cd552cbdb3bc942e575
Jari (LotU) <j.koopman@jarict.nl>
2020-01-15 23:05:42 +0100

    Get summary up to spec (#26)
    * Get summary up to spec
    
    * Update linuxmain

--- Tests/SwiftPrometheusTests/SwiftPrometheusTests.swift
+++ Tests/SwiftPrometheusTests/SwiftPrometheusTests.swift
@@ -15,19 +15,6 @@ final class SwiftPrometheusTests: XCTestCase {
         }
     }
     
-    struct BaseSummaryLabels: SummaryLabels {
-        var quantile: String = ""
-        let myValue: String
-        
-        init() {
-            self.myValue = "*"
-        }
-        
-        init(myValue: String) {
-            self.myValue = myValue
-        }
-    }
-    
     var prom: PrometheusClient!
     
     override func setUp() {
@@ -64,19 +51,5 @@ final class SwiftPrometheusTests: XCTestCase {
         self.prom.collect { metricsString in
             XCTAssertEqual(metricsString, "# HELP my_counter Counter for testing\n# TYPE my_counter counter\nmy_counter 30\nmy_counter{myValue=\"labels\"} 30")
         }
-    }
-    
-    func testSummary() {
-        let summary = prom.createSummary(forType: Double.self, named: "my_summary", helpText: "Summary for testing", quantiles: [0.5, 0.9, 0.99], labels: BaseSummaryLabels.self)
-        let summaryTwo = prom.createSummary(forType: Double.self, named: "my_summary", helpText: "Summary for testing", quantiles: [0.5, 0.9, 0.99], labels: BaseSummaryLabels.self)
-        
-        summary.observe(1)
-        summary.observe(2)
-        summary.observe(4)
-        summaryTwo.observe(10000)
-        
-        summary.observe(123, .init(myValue: "labels"))
-        
-        XCTAssertEqual(summary.collect(), "# HELP my_summary Summary for testing\n# TYPE my_summary summary\nmy_summary{quantile=\"0.5\", myValue=\"*\"} 4.0\nmy_summary{quantile=\"0.9\", myValue=\"*\"} 10000.0\nmy_summary{quantile=\"0.99\", myValue=\"*\"} 10000.0\nmy_summary_count{myValue=\"*\"} 5.0\nmy_summary_sum{myValue=\"*\"} 10130.0\nmy_summary{quantile=\"0.5\", myValue=\"labels\"} 123.0\nmy_summary{quantile=\"0.9\", myValue=\"labels\"} 123.0\nmy_summary{quantile=\"0.99\", myValue=\"labels\"} 123.0\nmy_summary_count{myValue=\"labels\"} 1.0\nmy_summary_sum{myValue=\"labels\"} 123.0")
     }
 }
