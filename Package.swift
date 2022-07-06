f4548753c0f1f23b98aff9b5c9485bb6b98582bc
Svilen Kirov <s.kirov@tum.de>
2022-07-06 10:07:18 +0300

    Added minimum iOS platform requirement, as it couldn't be used on iOS projects otherwise

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPrometheus",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "SwiftPrometheus",
