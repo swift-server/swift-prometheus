7a8e280c01721a8ed856334f17f68d61e8e051bc
freefr4ll <103502659+freef4ll@users.noreply.github.com>
2023-10-26 16:14:04 +0300

    SwiftNIO -> SwiftPrometheus license fix up

--- dev/update-benchmark-tresholds.sh
+++ dev/update-benchmark-tresholds.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ##===----------------------------------------------------------------------===##
 ##
-## This source file is part of the SwiftNIO open source project
+## This source file is part of the SwiftPrometheus open source project
 ##
-## Copyright (c) 2023 Apple Inc. and the SwiftNIO project authors
+## Copyright (c) 2023 Apple Inc. and the SwiftPrometheus project authors
 ## Licensed under Apache License v2.0
 ##
 ## See LICENSE.txt for license information
-## See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+## See CONTRIBUTORS.txt for the list of SwiftPrometheus project authors
 ##
 ## SPDX-License-Identifier: Apache-2.0
 ##
